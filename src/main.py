7b36eb919180deb3b1ed329cb8ced984e7853274
Nishigori <54665270+watagori@users.noreply.github.com>
2022-05-27 10:11:57 +0900

    feat: make caaj from data like bitbank (#21)
    * feat: set poetry path
    
    * feat: add space
    
    * feat: format document
    
    * fix: set path correctly
    
    * fix: add ``` in readme
    
    * feat: enable venv in docker
    
    * merge
    
    * feat: enable venv in docker
    
    * fix: delete unused ```
    
    * feat: add space before 'osmosis_plugin'
    
    * feat: add build-system
    
    * feat: delete osmosis-plugin
    
    * feat: use senkalib with buidl-system version
    
    * feat: make caaj from csv
    
    * feat: test senka if create correct caaj
    
    * feat: make unknowtransaction
    
    * feat: add csv to git for test
    
    * feat: for local execute
    
    * feat: get transaction form data
    
    * feat: get transaction form data
    
    * feat: get_caaj test
    
    * feat: test get_caaj_from_data
    
    * feat: add address or csv data into transaction_params
    
    * feat: add address or csv data into transaction_params
    
    * feat: add settings params
    
    * feat: add settings params
    
    * fix: change else indent
    
    * feat: delete result
    
    * feat: update senkalib
    
    * feat: delete unnecessary data file
    
    * feat: add *.csv in .gitognore
    
    * feat: use abs and rel path to run main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,36 @@
+import argparse
 import os
-import sys
+from pathlib import Path
 
 from senka.senka import Senka
 
 if __name__ == "__main__":
-    args = sys.argv
-    chain = args[1]
-    address = args[2]
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--chain",
+        help="--chain [chain]",
+    )
+    parser.add_argument(
+        "--address",
+        help="--address [address]",
+    )
+    parser.add_argument(
+        "--data_path",
+        help="--data_path [address]",
+    )
     setting = {}
-    if chain == "bsc":
-        setting["bscscan_key"] = os.environ["BSCSCAN_KEY"]
-    senka = Senka(setting, "./pyproject.toml")
-    caaj = senka.get_caaj_csv(chain, address)
+    args = parser.parse_args()
+    if args.chain is not None and args.address is not None:
+        if args.chain == "bsc":
+            setting["bscscan_key"] = os.environ["BSCSCAN_KEY"]
+        senka = Senka(setting, "./pyproject.toml")
+        caaj = senka.get_caaj_csv("address", args.chain, args.address)
+    elif args.chain is not None and args.data_path is not None:
+        senka = Senka(setting, "./pyproject.toml")
+        data = Path(f"{os.path.abspath(args.data_path)}").read_text()
+        caaj = senka.get_caaj_csv("csv", args.chain, data)
+    else:
+        raise Exception("Invalid arguments")
+
     print(caaj)
--- src/senka/senka.py
+++ src/senka/senka.py
@@ -22,20 +22,26 @@ class Senka:
 
     def get_caaj_csv(
         self,
+        data_type: str,
         chain: str,
-        address: str,
+        data: str,
         starttime: Union[int, None] = None,
         endtime: Union[int, None] = None,
         startblock: Union[int, None] = None,
         endblock: Union[int, None] = None,
     ) -> str:
+        transaction_params = {
+            "type": data_type,
+            "data": data,
+            "starttime": starttime,
+            "endtime": endtime,
+            "startblock": startblock,
+            "endblock": endblock,
+        }
+
         caaj_list, unknown_transactions_list = self.get_caaj(
             chain,
-            address,
-            starttime=starttime,
-            endtime=endtime,
-            startblock=startblock,
-            endblock=endblock,
+            transaction_params,
         )
         caaj_dict_list = list(map(lambda x: vars(x), caaj_list))
         df = pd.DataFrame(caaj_dict_list)
@@ -46,35 +52,50 @@ class Senka:
     def get_caaj(
         self,
         chain: str,
-        address: str,
-        starttime: Union[int, None] = None,
-        endtime: Union[int, None] = None,
-        startblock: Union[int, None] = None,
-        endblock: Union[int, None] = None,
+        transaction_params: dict,
     ) -> Tuple[List[CaajJournal], List[UnknownTransaction]]:
         token_original_ids = TokenOriginalIdTable(Senka.TOKEN_ORIGINAL_IDS_URL)
         available_chains = self.get_available_chains()
         if chain.lower() not in available_chains:
             raise ValueError("this chain is not supported.")
 
-        caaj = []
-        unknown_transactions = []
         transaction_generator = list(
             filter(
                 lambda x: x.chain.lower() == chain.lower(),
                 SenkaLib.get_available_chain(),
             )
         )[0]
-        transactions = transaction_generator.get_transactions(
-            self.setting,
-            address,
-            starttime=starttime,
-            endtime=endtime,
-            startblock=startblock,
-            endblock=endblock,
-        )
-        plugins = PluginManager.get_plugins(chain, self.setting_toml_path)
 
+        transaction_params["settings"] = self.setting
+        transactions = transaction_generator.get_transactions(transaction_params)
+        plugins = PluginManager.get_plugins(chain, self.setting_toml_path)
+        address = ""
+        if transaction_params["type"] == "address":
+            address = transaction_params["data"]
+
+        elif transaction_params["type"] == "csv":
+            address = "self"
+
+        return Senka._make_caaj_from_transaction_and_plugins(
+            transactions, plugins, token_original_ids, chain, address
+        )
+
+    @staticmethod
+    def get_available_chains() -> List[str]:
+        chains = SenkaLib.get_available_chain()
+        chains = list(map(lambda x: x.chain, chains))
+        return chains
+
+    @staticmethod
+    def _make_caaj_from_transaction_and_plugins(
+        transactions: List[CaajJournal],
+        plugins: list,
+        token_original_ids: TokenOriginalIdTable,
+        chain: str,
+        address: str,
+    ) -> Tuple[List[CaajJournal], List[UnknownTransaction]]:
+        caaj = []
+        unknown_transactions = []
         for transaction in transactions:
             for plugin in plugins:
                 if plugin.can_handle(transaction):
@@ -83,19 +104,13 @@ class Senka:
                     )
                     caaj.extend(caaj_peace)
                     break
-            else:
-                unknown_transactions.append(
-                    UnknownTransaction(
-                        chain,
-                        address,
-                        transaction.transaction_id,
-                        "there is no applicable plugin",
+                else:
+                    unknown_transactions.append(
+                        UnknownTransaction(
+                            chain,
+                            address,
+                            transaction.transaction_id,
+                            "there is no applicable plugin",
+                        )
                     )
-                )
-
         return caaj, unknown_transactions
-
-    def get_available_chains(self) -> List[str]:
-        chains = SenkaLib.get_available_chain()
-        chains = list(map(lambda x: x.chain, chains))
-        return chains
