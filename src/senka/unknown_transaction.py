eb28ddc79b81e793608f9808600d940d2202492b
Kou Matsumoto <develop@kou-matsumoto.jp>
2022-06-14 11:53:15 +0900

    feat: add UnknownTransaction executed_at property

--- src/senka/unknown_transaction.py
+++ src/senka/unknown_transaction.py
@@ -1,6 +1,14 @@
 class UnknownTransaction:
-    def __init__(self, chain: str, address: str, transaction_id: str, reason: str):
+    def __init__(
+        self,
+        chain: str,
+        address: str,
+        transaction_id: str,
+        reason: str,
+        executed_at: str,
+    ):
         self.chain = chain
         self.address = address
         self.transaction_id = transaction_id
         self.reason = reason
+        self.executed_at = executed_at
